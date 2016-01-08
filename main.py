4447e979eea245d45dc813d63c7239355ff68fe3
AnandrewChan <443919544@qq.com>
2016-01-08 22:19:30 +0800

    有了管理员

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ class Application(tornado.web.Application):
             (r'/',HomeHandler),#主页
             (r'/user/reg',RegisterHandler),#注册
             (r'/user/login',LoginHandler),#登录
+            (r'/admin',AdminHandler),
             (r'/admin/login',AdminLoginHandler),#管理员登录
             (r'/user/remove',DeleteUserHandler),#管理员删除用户
             (r'/user/all',AllUserHandler),#管理员删除用户
@@ -76,6 +77,10 @@ class ShopChartHandler(tornado.web.RequestHandler):
     def get(self):
         user = self.get_current_user()
         self.render('shoppingchart.html',user=user)
+class AdminHandler(BaseHandler):
+    def get(self):
+        user = self.get_current_admin()
+        self.render('index.html',user=user)
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     Application().listen(options.port)
