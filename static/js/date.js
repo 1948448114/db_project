777d3388142c50ab553e293edf535217aa71d8f0
AnandrewChan <443919544@qq.com>
2016-01-08 23:43:05 +0800

    有点问题啊啊啊啊

--- static/js/date.js
+++ static/js/date.js
@@ -1,104 +1,90 @@
+function setTab(name, cursel, num) {
 
-function setTab(name,cursel){
+	cursel_0 = cursel;
+	if (num >= links_len) {
+		links_len = num;
+	}
+	for (var i = 1; i <= links_len; i++) {
 
-cursel_0=cursel;
+		var menu = document.getElementById(name + i);
+		var medo = document.getElementById(name + i);
+		var medo1 = document.getElementById(name + i);
+		var medo2 = document.getElementById(name + i);
 
-for(var i=1; i<=links_len; i++){
+		var meduo2 = document.getElementById("con_" + name + "_" + i);
+		var meduo1 = document.getElementById("con_" + name + "_" + i);
+		var menudiv = document.getElementById("con_" + name + "_" + i);
+		var meduo = document.getElementById("con_" + name + "_" + i);
+		if (i == cursel) {
 
-var menu = document.getElementById(name+i);
-var medo = document.getElementById(name+i);
-var medo1 = document.getElementById(name+i);
-var medo2 = document.getElementById(name+i);
+			menu.className = "off";
+			medo.className = "off";
+			medo1.className = "off";
+			medo2.className = "off";
 
-var meduo2 = document.getElementById("con_"+name+"_"+i);
-var meduo1 = document.getElementById("con_"+name+"_"+i);
-var menudiv = document.getElementById("con_"+name+"_"+i);
-var meduo = document.getElementById("con_"+name+"_"+i);
-if(i==cursel){
+			meduo2.style.display = "block";
+			meduo1.style.display = "block";
+			meduo.style.display = "block";
+			menudiv.style.display = "block";
 
-menu.className="off";
-medo.className="off";
-medo1.className="off";
-medo2.className="off";
+		} else {
 
-meduo2.style.display="block";
-meduo1.style.display="block";
-meduo.style.display="block";
-menudiv.style.display="block";
+			menu.className = " ";
+			medo.className = " ";
+			medo2.className = " ";
+			medo1.className = " ";
 
-}
+			meduo1.style.display = "none";
+			meduo2.style.display = "none"
+			meduo.style.display = "none";
+			menudiv.style.display = "none";
 
-else{
+		}
 
-menu.className="";
-medo.className="";
-medo2.className="";
-
-medo1.className="";
-
-meduo1.style.display="none";
-
-meduo2.style.display="none"
-meduo.style.display="none";
-menudiv.style.display="none";
-
-}
-
-}
+	}
 
 }
 
-function Next(){
+function Next() {
 
-cursel_0++;
+	cursel_0++;
 
-if (cursel_0>links_len)cursel_0=1
+	if (cursel_0 > links_len) cursel_0 = 1
 
-setTab(name_0,cursel_0);
-
-}
-
-var name_0='one';
-var name_0='column';
-var name_0='colum';
-var name_0='colunm';
-var name_0='colnm';
-
-var cursel_0=1;
-
-var links_len,iIntervalId;
-
-onload=function(){
-
-var links = document.getElementById("tab1").getElementsByTagName('li')
-var links = document.getElementById("tab2").getElementsByTagName('dd')
-var links = document.getElementById("tab3").getElementsByTagName('dd')
-var links = document.getElementById("tab4").getElementsByTagName('dd')
-
-
-links_len=links.length;
-
-for(var i=0; i<links_len; i++){
-
-links[i].onmouseover=function(){
-
-clearInterval(iIntervalId);
+	setTab(name_0, cursel_0,0);
 
 }
 
+var name_0 = 'one';
+var name_0 = 'column';
+var name_0 = 'colum';
+var name_0 = 'colunm';
+var name_0 = 'colnm';
+
+var cursel_0 = 1;
+
+var links_len, iIntervalId;
+
+onload = function() {
+
+	var links = document.getElementById("tab2").getElementsByTagName('dd')
+	var links = document.getElementById("tab3").getElementsByTagName('dd')
+	var links = document.getElementById("tab4").getElementsByTagName('dd')
+
+	var links = document.getElementById("tab1").getElementsByTagName('li')
+	links_len = links.length;
+
+	for (var i = 0; i < links_len; i++) {
+
+		links[i].onmouseover = function() {
+
+			clearInterval(iIntervalId);
+
+		}
+
+	}
+
+
+	setTab(name_0, cursel_0,0);
+
 }
-
-document.getElementById("con_"+name_0+"_"+links_len).parentNode.onmouseover=function(){
-
-clearInterval(iIntervalId);
-
-}
-
-setTab(name_0,cursel_0);
-
-}
-
-
-
-
-
