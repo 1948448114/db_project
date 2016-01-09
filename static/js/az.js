6288034a49541046813d11d7decab38e4a1f6c5b
TracyWei <tracy_wei7@163.com>
2016-01-09 20:39:12 +0800

    哇有第一页了

--- static/js/az.js
+++ static/js/az.js
@@ -1,3 +1,29 @@
+String.prototype.replaceAll = function (exp, newStr) {
+    return this.replace(new RegExp(exp, "gm"), newStr);
+};
+/**
+ * 原型：字符串格式化
+ * @param args 格式化参数值
+ */
+String.prototype.format = function(args) {
+    var result = this;
+    if (arguments.length < 1) {
+        return result;
+    }
+
+    var data = arguments; // 如果模板参数是数组
+    if (arguments.length == 1 && typeof (args) == "object") {
+        // 如果模板参数是对象
+        data = args;
+    }
+    for ( var key in data) {
+        var value = data[key];
+        if (undefined != value) {
+            result = result.replaceAll("\\{" + key + "\\}", value);
+        }
+    }
+    return result;
+}
 $(function(){
 	// $.ajax({
 	// 	url: '/user/login',
@@ -148,6 +174,35 @@ $('#signup_btn').click(function(event){
 			$('#signup_alert>p').html(data.content);
 		});
 	});
+//————————————首页书籍展示————————————
+$.ajax({
+	url: '/book/all',
+	type: 'POST',
+	dataType: 'json',
+	data: {page: 1,pagesize:5},
+})
+.done(function(data) {
+	console.log(data.content[1].picture);
+	var template2="我是{name}，今年{age}了";
+	var result2=template2.format({name:"loogn",age:22});
+	console.log(result2);
+	for(var i = 0; i<data.content.length ;i++){
+
+	var homeTemplate='<html><head></head><body><li class="list-group-item"><div class="row"><div class="col-sm-3 bookImg"><img src="{picture}"></div><div class="col-sm-3"><div class="row">书籍名称:&nbsp;&nbsp; <span class="home_name">{name}</span></div><div class="row">&nbsp;ISBN:&nbsp;&nbsp; <span class="home_isbn">{isbn}</span></div><div class="row">上架时间:&nbsp;&nbsp; <span id="home_shelftime">{shelftime}</span></div></div><div class="col-sm-3"><div id="home_" class="row">作者:&nbsp;&nbsp; <span id="home_autor">{author}</span></div><div id="home_" class="row">出版时间:&nbsp;&nbsp; <span id="home_releasetime">{releasetime}</span></div><div class="row">销售额:&nbsp;&nbsp; <span id="home_soldnum">{soldnum}</span></div></div><div class="col-sm-3"><div class="row">余量:&nbsp;&nbsp; <span id="home_remainnum">{remainnum}</span></div><div class="row">价格:&nbsp;&nbsp; <span id="home_price">{price}</span></div><div class="row">状态:&nbsp;&nbsp; <span id="home_active">{active}</span></div><div class="row"><button class="btn btn-danger btn-sm">加入购物车</button> <button class="btn btn-warning btn-sm showInfo">查看简介</button></div></div></div><div class="row bookInfo"><h5>书籍简介:</h5><p id="home_note">{note}</p></div></li></body></html>';
+	var result=homeTemplate.format({picture:data.content[i].picture,name:data.content[i].name,isbn:data.content[i].isbn,shelftime:data.content[i].shelftime,
+		author:data.content[i].author,releasetime:data.content[i].releasetime,soldnum:data.content[i].soldnum,remainnum:data.content[i].remainnum,
+		price:data.content[i].price,active:data.content[i].active,note:data.content[i].note});
+	console.log(result);
+	$('#homeBookUl').append(result);
+	}
+})
+.fail(function() {
+	console.log("error");
+})
+.always(function() {
+	console.log("complete");
+});
+
 })
 
 function addNum(){
